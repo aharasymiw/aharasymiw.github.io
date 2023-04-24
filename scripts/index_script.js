536eeedf2ce49369702c6572def58599b450f591
Andrew Harasymiw <aharasymiw@gmail.com>
2023-04-23 19:02:40 -0700

    update a bunch of stuff, new page structure, add jquery, dynamically add nav and footer.

---
+++ scripts/index_script.js
@@ -0,0 +1,66 @@
+console.log("works!");
+
+$(document).ready(onReady);
+
+function onReady() {
+    addNav();
+
+    addFooter();
+}
+
+function addNav() {
+    console.log("addNav");
+
+    let navHtmlString =
+        `
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="views/passtimes.html">Passtimes</a></li>
+                <li><a href="views/personal.html">Personal</a></li>
+                <li><a href="views/professional.html">Professional</a></li>
+                <li><a href="views/projects.html">Projects</a></li>
+            </ul>
+        </nav>
+
+        <h1 class="FirstName">Andrew</h1>
+        <h1 class="LastName">Harasymiw</h1>
+        <h2>Developer for hire</h2>
+        `
+
+    $('header').append(navHtmlString);
+}
+
+function addFooter() {
+    console.log("addNav");
+
+    let navHtmlString =
+        `
+        <p>&#169; 2023 by Andrew Harasymiw.</p>
+        <nav>
+            <ul>
+                <li>
+                    <a href="https://github.com/aharasymiw">
+                        <img class=".social-image" src="./images/github_logo.svg"
+                                alt="Github Logo, and link to github." height="20" width="20">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/aharasymiw" class=".image-link">
+                        <img class=".social-image" src="./images/linkedin_logo.svg"
+                                alt="LinkedIn Logo, and link to LinkedIn." height="20" width="20">
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:aharasymiw@gmail.com" class=".image-link">
+                        <img class=".social-image" src="./images/email_icon.svg"
+                                alt="Email icon, and link to send an email." height="20" width="20">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        `
+
+    $('footer').append(navHtmlString);
+
+}
