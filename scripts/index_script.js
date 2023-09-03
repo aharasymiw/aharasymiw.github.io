2520cf0382f3e4cca3f744da9242e9353e3be8ea
Andrew Harasymiw <aharasymiw@gmail.com>
2023-09-02 19:45:12 -0700

    I don't know what I want to do with this site, but at least the nav and footer html isn't replicated anymore.

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -1,72 +1,14 @@
-console.log("works!");
+console.log('works!');
 
 $(document).ready(onReady);
 
 function onReady() {
-    addNav();
+    addNav(indexPathPrefix, navPathPrefix);
 
-    addFooter();
+    addFooter(imagePathPrefix);
 }
 
-function addNav() {
-    console.log("addNav");
+let indexPathPrefix = '';
+let navPathPrefix = 'views/';
 
-    let navHtmlString =
-        `
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="views/passtimes.html">Passtimes</a></li>
-                <li><a href="views/personal.html">Personal</a></li>
-                <li><a href="views/professional.html">Professional</a></li>
-                <li><a href="views/projects.html">Projects</a></li>
-            </ul>
-        </nav>
-
-        <h1 class="FirstName">Andrew</h1>
-        <h1 class="LastName">Harasymiw</h1>
-        <h2>Developer for hire</h2>
-        `
-
-    $('header').append(navHtmlString);
-}
-
-function addFooter() {
-    console.log("addNav");
-
-    let navHtmlString =
-        `
-        <p>&#169; 2023 by Andrew Harasymiw.</p>
-        <nav>
-            <ul>
-                <li>
-                    <a href="https://github.com/aharasymiw">
-                        <img class=".social-image" src="../images/github_logo.svg"
-                                alt="Github Logo, and link to github." height="20" width="20">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/aharasymiw" class=".image-link">
-                        <img class=".social-image" src="../images/linkedin_logo.svg"
-                                alt="LinkedIn Logo, and link to LinkedIn." height="20" width="20">
-                    </a>
-                </li>
-                <li>
-                    <a href="https://hachyderm.io/@aharasymiw" rel="me" class=".image-link">
-                        <img class=".social-image" src="../images/mastodon_logo.svg"
-                                alt="Mastadon Logo, and link to https://hachyderm.io/@aharasymiw on Mastadon." height="20" width="20">
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:aharasymiw@gmail.com" class=".image-link">
-                        <img class=".social-image" src="../images/email_icon.svg"
-                                alt="Email icon, and link to send an email." height="20" width="20">
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        `
-
-    $('footer').append(navHtmlString);
-
-}
+let imagePathPrefix = '';
