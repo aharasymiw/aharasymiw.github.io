5eceef3d00b11f9d9b93d1ccad94f226c41d4f19
Andrew Harasymiw <aharasymiw@gmail.com>
2026-04-07 16:17:03 -0700

    update vite.config.ts output directory to /dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite-plus";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/dist",
   plugins: [react()],
   staged: {
     "*": "vp check --fix",
